9353550c030e52acae6f318097f414d25acb3eab
Kleber Fellipe <133381521+KleberFellipe@users.noreply.github.com>
2024-06-13 13:12:25 +0000

    13/06/2024

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-function search(){
+function search() {
   let input = document.getElementById('searchbar').value
   input = input.toLowerCase()
   let x = document.getElementsByClassName('procura')
-  for(i = 0; i < x.length; i++){if(!x[i].innerHTML.toLowerCase().includes(input)){
-    x[i].style.display = "none"
-}else{
-    x[i].style.display = "list-item"
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none"
+    } else {
+      x[i].style.display = "list-item";
+    }
   }
 }
+if(input === ''){
+  for(i = 0; i < x.lenght; i++) {
+    x[i].style.display="none"
+  }
+}
+else{
+  for (i = 0; i < x.lenght; i++){
+    if (!x[i].innerHTML.toLowerCase().includes(input)){
+      
+    }
+    else{
+      x[i].style.display="list-item";
+    }
+  }
 }
